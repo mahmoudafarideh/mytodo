b150e2118f8835759f18fff422e9a23758f51c40
Mahmoud Afarideh <mahmoudafarideh@gmail.com>
2024-03-29 01:50:34 +0330

    Setups the Project

---
+++ app/src/main/java/com/ma/mytodo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ma.mytodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ma.mytodo.ui.add.AddTodoViewModel
+import com.ma.mytodo.ui.theme.MyTodoTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTodoTheme {
+            }
+        }
+    }
+}
